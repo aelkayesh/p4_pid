6deee63f782ea4ddbc2a0b23ecb123cc8e05c523
ElKayesh, Ahmed <Ahmed.ElKayesh@nuance.com>
2018-09-12 21:42:00 +0200

    first commit

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,4 +1,7 @@
 #include "PID.h"
+#include<limits>
+#include<numeric>
+#include<iostream>
 
 using namespace std;
 
@@ -11,11 +14,34 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+	p[0] = Kp;
+	p[1] = Ki;
+	p[2] = Kd;
+	best_error = numeric_limits<double>::max();
+	current_index = 2;//p
+	initialized = true;
 }
 
 void PID::UpdateError(double cte) {
+	if (step == 0) {
+		prev_cte = cte;
+	}
+	p_error = cte;
+	i_error += cte;
+	d_error = cte - prev_cte;
+	prev_cte = cte;
+	step++;
 }
 
 double PID::TotalError() {
+	return	-p[0] * p_error
+			- p[1] * i_error
+			- p[2] * d_error;
 }
+
+bool PID::isInitialized() {
+	return initialized;
+}
+	 
+
 
--- src/PID.h
+++ src/PID.h
@@ -1,21 +1,37 @@
 #ifndef PID_H
 #define PID_H
+#include <vector>
 
 class PID {
 public:
   /*
   * Errors
   */
-  double p_error;
-  double i_error;
-  double d_error;
+	double p_error;
+	double i_error;
+	double d_error;
 
+  double dp[3] = {1,1,1};
+  
   /*
   * Coefficients
   */ 
-  double Kp;
-  double Ki;
-  double Kd;
+  double p[3] = {0,0,0};
+  
+
+  //Needed to calculate average CTE
+  std::vector<int> cte_list;
+
+  double best_error;
+  double prev_cte;
+  int step = 0;
+
+  //current error parameter to optimize, p,i or d
+  int current_index;
+
+  //resume calculating error for the same error param, p, i or d
+  bool resumeFromNestedElse = false;
+  bool initialized = false;
 
   /*
   * Constructor
@@ -41,6 +57,9 @@ public:
   * Calculate the total PID error.
   */
   double TotalError();
+
+  //returns true if pid is initialized
+  bool isInitialized();
 };
 
 #endif /* PID_H */
